a51796965b9f01ce3052941609b81cfe45af189e
Sahin Vardar <sahin@vardar.io>
2023-05-29 18:35:48 +0200

    chore: passing ident options across generate

--- src/print-html.ts
+++ src/print-html.ts
@@ -33,7 +33,7 @@ class FragmentPrinter extends BaseHtmlNodePrinter {
 }
 
 class ElementPrinter extends BaseHtmlNodePrinter {
-  attributeValueToString(attribute: Attribute | SpreadAttribute) {
+  attributeValueToString(attribute: Attribute | SpreadAttribute, context: PrinterContext) {
     if (attribute.value) {
       const [value] = attribute.value;
 
@@ -42,12 +42,12 @@ class ElementPrinter extends BaseHtmlNodePrinter {
       }
 
       if (['MustacheTag', 'AttributeShorthand'].includes(value.type)) {
-        return `{${generate(value.expression)}}`;
+        return `{${generate(value.expression, context.indent)}}`;
       }
     }
 
     if (attribute.expression) {
-      return `{${generate(attribute.expression)}}`;
+      return `{${generate(attribute.expression, context.indent)}}`;
     }
 
     return '';
@@ -105,7 +105,7 @@ class ElementPrinter extends BaseHtmlNodePrinter {
           }
 
           if (value && value.type === 'AttributeShorthand') {
-            return this.attributeValueToString(attribute as Attribute);
+            return this.attributeValueToString(attribute as Attribute, context);
           }
 
           if (attribute?.expression === null) {
@@ -113,23 +113,23 @@ class ElementPrinter extends BaseHtmlNodePrinter {
           }
 
           if (attribute.type === 'Spread') {
-            return `{...${generate(attribute.expression)}}`;
+            return `{...${generate(attribute.expression, context.indent)}}`;
           }
 
           if (attribute?.expression) {
             return `${attributeName}={${generate(attribute.expression, context.indent)}}`;
           }
 
-          return `${attributeName}=${this.attributeValueToString(attribute as Attribute)}`;
+          return `${attributeName}=${this.attributeValueToString(attribute as Attribute, context)}`;
         })
         .join(' ') ?? '';
 
     if (elementNode.expression) {
-      write(` this={${generate(elementNode.expression)}}`);
+      write(` this={${generate(elementNode.expression, context.indent)}}`);
     }
 
     if (elementNode.tag) {
-      write(` this={${generate(elementNode.tag)}}`);
+      write(` this={${generate(elementNode.tag, context.indent)}}`);
     }
 
     write(attributes !== '' ? ` ${attributes}` : '');
@@ -198,7 +198,7 @@ class NoOpPrinter extends BaseHtmlNodePrinter {
 class MustacheTagPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const mustacheTagNode = node as MustacheTag;
-    context.write(`{${generate(mustacheTagNode.expression)}}`);
+    context.write(`{${generate(mustacheTagNode.expression, context.indent)}}`);
 
     context._this.replace({
       ...node,
@@ -269,10 +269,10 @@ class EachBlockPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
 
-    write(`{#each ${generate(node.expression)}`);
+    write(`{#each ${generate(node.expression, context.indent)}`);
 
     if (node.context) {
-      write(` as ${generate(node.context)}`);
+      write(` as ${generate(node.context, context.indent)}`);
     }
 
     if (node.index) {
@@ -280,7 +280,7 @@ class EachBlockPrinter extends BaseHtmlNodePrinter {
     }
 
     if (node.key) {
-      write(` (${generate(node.key)})`);
+      write(` (${generate(node.key, context.indent)})`);
     }
 
     write('}');
@@ -301,7 +301,7 @@ class EachBlockPrinter extends BaseHtmlNodePrinter {
 class AwaitBlockPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
-    write(`{#await ${generate(node.expression)}}`);
+    write(`{#await ${generate(node.expression, context.indent)}}`);
   }
   leave(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
@@ -319,7 +319,7 @@ class ThenBlockPrinter extends BaseHtmlNodePrinter {
     const { write } = context;
     write('{:then');
     if (parent.value) {
-      write(` ${generate(parent.value)}`);
+      write(` ${generate(parent.value, context.indent)}`);
     }
     write('}');
   }
@@ -331,7 +331,7 @@ class CatchBlockPrinter extends BaseHtmlNodePrinter {
     const { write } = context;
     write('{:catch');
     if (parent.error) {
-      write(` ${generate(parent.error)}`);
+      write(` ${generate(parent.error, context.indent)}`);
     }
     write('}');
   }
@@ -341,7 +341,7 @@ class CatchBlockPrinter extends BaseHtmlNodePrinter {
 class KeyBlockPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
-    write(`{#key ${generate(node.expression)}}`);
+    write(`{#key ${generate(node.expression, context.indent)}}`);
   }
   leave(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
@@ -352,7 +352,7 @@ class KeyBlockPrinter extends BaseHtmlNodePrinter {
 class RawMustacheTagPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
-    write(`{@html ${generate(node.expression)}}`);
+    write(`{@html ${generate(node.expression, context.indent)}}`);
     context._this.replace({
       ...node,
       expression: undefined
@@ -366,7 +366,7 @@ class RawMustacheTagPrinter extends BaseHtmlNodePrinter {
 class DebugTagPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
-    write(`{@debug ${node.identifiers.map((id: any) => generate(id)).join(', ')}}`);
+    write(`{@debug ${node.identifiers.map((id: any) => generate(id, context.indent)).join(', ')}}`);
   }
   leave(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
@@ -376,7 +376,7 @@ class DebugTagPrinter extends BaseHtmlNodePrinter {
 class ConstTagPrinter extends BaseHtmlNodePrinter {
   enter(node: TemplateNode, parent: TemplateNode, context: PrinterContext) {
     const { write } = context;
-    write(`{@const ${generate(node.expression)}}`);
+    write(`{@const ${generate(node.expression, context.indent)}}`);
     context._this.replace({
       ...node,
       expression: undefined
