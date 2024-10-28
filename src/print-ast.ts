9c8dbb0d04246adf9461569408be1f8220fefba2
Sahin Vardar <sahin@vardar.io>
2024-10-28 15:27:58 +0100

    WIP

--- src/print-ast.ts
+++ src/print-ast.ts
@@ -1,31 +1,9 @@
-import type {  AST } from 'svelte/src/compiler/types/template.js';
+import type { AST } from 'svelte/src/compiler/types/template.js';
 import { DefaultPrinterIdentOptions, PrinterIdentOptions, printHtml, printScript } from './index.js';
 
-export interface PrintAstParams {
-  ast: AST.Root;
-  indent?: PrinterIdentOptions;
-}
+export default function printAst(root: AST.Root, indent: PrinterIdentOptions = DefaultPrinterIdentOptions): string {
+  const markup = printHtml(root, indent);
+  const script = printScript(root, indent);
 
-export default function printAst(params: PrintAstParams): string {
-  const { ast } = params;
-  const ident = {
-    ...DefaultPrinterIdentOptions,
-    ...params.indent
-  };
-
-  const markup = printHtml({
-    rootNode: ast.html,
-    ident
-  });
-
-  const scriptInstance = ast.instance ? printScript({ script: ast.instance, ident }) : '';
-  const scriptModule = ast.module ? printScript({ script: ast.module, ident }) : '';
-
-  let result = '';
-
-  result += scriptModule + ident.lineEnd;
-  result += scriptInstance + ident.lineEnd;
-  result += markup;
-
-  return result;
+  return script + indent.lineEnd + markup;
 }
